ba124f9339225e77093fef88ca1bf4b16dc24bbd
daltonpearson <32880838+daltonpearson@users.noreply.github.com>
2017-10-19 12:16:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	
+	function fullscreen() {
+		$('a').click(function() {
+			if(!$(this).hasClass('noeffect')) {
+				window.location = $(this).attr('href');
+				return false;
+			}
+		});
+	}
+	
+	fullscreen();
+});
